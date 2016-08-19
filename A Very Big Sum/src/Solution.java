fd6e4b78c38b44a234f73d2177a237d76c77febc
Metroxe <chrispow96@gmail.com>
2016-08-19 15:04:32 -0700

    A Very Big Sum

--- A Very Big Sum/src/Solution.java
+++ A Very Big Sum/src/Solution.java
@@ -5,10 +5,9 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int result = 0;
+        long result = 0;
         for (int i = 0; i < n; i++) {
             result += in.nextInt();
-
         }
         System.out.println(result);
     }
