ded24440ceabf4b9d53bffe17267c1adee080919
Metroxe <chrispow96@gmail.com>
2016-08-27 12:15:08 -0700

    Tree PreorderTraversal Redone

--- Tree Preorder Traversal/Solution.java
+++ Tree Preorder Traversal/Solution.java
@@ -9,35 +9,12 @@ Node right;
 
 */
 
-        void preOrder(Node root){
-                int i = 0;
-                ArrayList<Node> arr = new ArrayList<Node>();
-        arr.add(i,root);
-        String result = Integer.toString(arr.get(i).data);
-        Node cur = root;
-        Node last = null;
+void preOrder(Node root) {
+	if (root == null) {
+		return;
+	}
 
-        while (true) {
-        if (cur.left != null && !arr.contains(cur.left)) {
-        i+=1;
-        cur = cur.left;
-        arr.add(i,cur);
-        result += " " + Integer.toString(arr.get(i).data);
-
-        } else if (cur.right !=null && !arr.contains(cur.right)) {
-        i+=1;
-        cur = cur.right;
-        arr.add(i,cur);
-        result += " " + Integer.toString(arr.get(i).data);
-
-        } else if (cur != root) {
-        i-=1;
-        cur = arr.get(i);
-
-        } else {
-        break;
-        }
-        }
-
-        System.out.println(result);
-        }
+	System.out.print(root.data + " ");
+	preOrder(root.left);
+	preOrder(root.right);
+}
