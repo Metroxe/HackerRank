31278297ea27e62920ad3f7dd2d746ec7d0d9b26
Metroxe <chrispow96@gmail.com>
2016-08-29 11:11:40 -0700

    Kangaroo

---
+++ Kangaroo/Solution.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int x1 = in.nextInt();
+		int v1 = in.nextInt();
+		int x2 = in.nextInt();
+		int v2 = in.nextInt();
+
+		while (x2>=x1){
+			if (x1==x2) {
+				System.out.println("YES");
+				return;
+			}
+			x1+=v1;
+			x2+=v2;
+		}
+
+		System.out.println("NO");
+
+	}
+}
