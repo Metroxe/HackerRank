8ce86cfc9aa4ae3f6163afbc0070eb61d0d22ea8
Metroxe <chrispow96@gmail.com>
2016-08-29 21:03:58 -0700

    Java Inheritance II

---
+++ Java Inheritance II/Solution.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Arithmetic{
+	Integer add(int a, int b) {
+		return(a+b);	
+	}
+}
+
+class Adder extends Arithmetic{}
